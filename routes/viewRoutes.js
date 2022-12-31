70f9c60b4bf91c50460f2f0d69f6dea9c6051add
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2022-12-31 15:27:45 -0500

    Added more content to pug templates

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,7 +3,6 @@ const viewController = require('./../controllers/viewControllers')
 const router = express.Router();
 
 router.get('/', viewController.getOverview)
-router.get('/tour', viewController.getTour)
-router.get('/tour/:slug', viewController.get)
+router.get('/tour/:slug', viewController.getTour)
 
 module.exports = router
